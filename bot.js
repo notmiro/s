1ece70500256252acbd54c4adf6728bbc5eca843
notmiro <45168643+notmiro@users.noreply.github.com>
2019-05-31 04:19:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 
-client.login("MzM3Mzk4ODQzNDU4OTc3Nzky.DwDKhQ.xJW3iixRVelFsq-0cG6GGbMHQ4M");
+client.login("NTQwOTY4NDE2NDU1MDMyODUw.XPCJ3w.os8TkwCF0ZEDZSJrNGv7wJ1ZwZ4");
 
 
 
@@ -25,35 +25,35 @@ client.user.setStatus("dnd");
 
 
 const devs = ["337398843458977792"];
-const adminprefix = ["mi"];
+const adminprefix = ["-"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('miply')) {
+  if (message.content.startsWith('-ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
      if (message.content === ("leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith('miwt')) {
+  if (message.content.startsWith('-wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('mils')) {
+  if (message.content.startsWith('-ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('mist')) {
+  if (message.content.startsWith('-st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith('misetname')) {
+  if (message.content.startsWith('-setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith('setavatar')) {
+if (message.content.startsWith('-setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
